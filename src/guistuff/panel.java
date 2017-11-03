4e623ba2a23053c97f2d46f532135f2827daea90
Kevin  Pham <phobdabam@business-cs.deanza.ad.fhda.edu>
2017-11-03 15:58:06 -0700

    ok

--- src/guistuff/panel.java
+++ src/guistuff/panel.java
@@ -2,23 +2,42 @@ package guistuff;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Timer;
 import javax.swing.JPanel;
 
-public class panel extends JPanel{
-	int x = 250,
-	    y = 250;
+public class panel extends JPanel implements ActionListener {
+	static int x = 50,
+	    y = 50;
+	int move = 2;
 	Graphics p;
+	Timer panTime = new Timer(10, this);
 	public panel() {
 		
 	}
 	public void paint (Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		g.setColor(Color.BLUE);
-		g.fillOval(x - 49, y - 49, 99, 99);
+		g.fillOval(x , y , 100, 100);
+		panTime.start();
 	}
 	public void pencil (int xpos, int ypos) {
 		x = xpos;
 		y = ypos;
+		repaint();
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		x = x + move;
+		
+		if (x == this.getWidth()-100) {
+			move = -2;
+		}
 		repaint();
 	}
 }
