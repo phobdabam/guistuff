ddadf30d4dd35c0fafef43a88e0cb0660316ff24
Kevin  Pham <phobdabam@business-cs.deanza.ad.fhda.edu>
2017-11-03 15:22:52 -0700

    ok

--- src/guistuff/APracticeTimer.java
+++ src/guistuff/APracticeTimer.java
@@ -1,10 +1,20 @@
 package guistuff;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.JFrame;
+
 public class APracticeTimer {
 	public static void main(String[] args) {
 		new JFrame().setVisible(true);
 		ActionListener actionListener = new ActionListener() {
-			
+			public void actionPerformed (ActionEvent actionEvent) {
+				System.out.println("Hello world timer");
+			}
 		}
+		Timer timer = new Timer(1000, actionListener);
+		timer.start();
 	}
 }
